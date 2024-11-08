3484934b2f17ebe1cacb39a37d1d969d407533d3
tarunsaisrinivasallam <tarun.allam@mhtechin.in>
2024-11-08 09:46:25 +0530

    dashboard update

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,7 +3,7 @@ import { Header } from "./_components/Header";
 
 function DashboardLayout({ children }) {
   return (
-    <div>
+    <div className="background-image md:h-screen ">
       <Header />
       <div className="mx-5 md:mx-20 lg:,mx:36">{children}</div>
     </div>
