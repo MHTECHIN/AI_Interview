5339c1c0c8ba3b16515bb53f8cc44c596e6c0fa2
AtchutaJampana <Atchuta.Jampana@mhtechin.in>
2024-11-11 12:39:12 +0530

    commit final

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -31,30 +31,28 @@ const Feedback = ({params}) => {
   return (
     <div className='p-10'>
       <h2 className='text-3xl font-bold text-green-600'>Congratulations!</h2>
-      <h2 className='font-bold text-2xl'>Here is your interview feedback</h2>
+      <h2 className='text-2xl font-bold'>Here is your interview feedback</h2>
       {feedbackList?.length ==0 ?
-      <h2 className='font-bold text-lg text-green-500'>No interview Feedback</h2>
+      <h2 className='text-lg font-bold text-green-500'>No interview Feedback</h2>
       : <>
-      <h2 className='text-primary text-lg my-2'>
-        Your overall interview rating: <strong>7/10</strong>
-      </h2>
+      
       <h2 className='text-sm text-gray-500'>Find below interview questions with coreect answers,Your answer and feedback for improvements for your next interview</h2>
       {feedbackList&&feedbackList.map((item,index)=>(
         <Collapsible key={index} className='mt-7'>
-        <CollapsibleTrigger className='p-2 flex justify-between bg-secondary rounded-lg my-2 text-left gap-7 w-full'>
+        <CollapsibleTrigger className='flex justify-between w-full p-2 my-2 text-left rounded-lg bg-secondary gap-7'>
         {item.question} <ChevronsUpDown className='h-4'/>
         </CollapsibleTrigger>
         <CollapsibleContent>
           <div className='flex flex-col gap-2'>
-            <h2 className='text-red-500 p-2 border rounded-lg'>
+            <h2 className='p-2 text-red-500 border rounded-lg'>
               <strong>
                 Rating:
               </strong>
               {item.rating}
             </h2>
-            <h2 className='p-2 border rounded-lg bg-red-50 text-sm text-red-900'><strong>Your Answer: </strong>{item.userAns}</h2>
-            <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer Looks Like: </strong>{item.correctAns}</h2>
-            <h2 className='p-2 border rounded-lg bg-blue-50 text-sm text-primary'><strong>Feedback: </strong>{item.feedback}</h2>
+            <h2 className='p-2 text-sm text-red-900 border rounded-lg bg-red-50'><strong>Your Answer: </strong>{item.userAns}</h2>
+            <h2 className='p-2 text-sm text-green-900 border rounded-lg bg-green-50'><strong>Correct Answer Looks Like: </strong>{item.correctAns}</h2>
+            <h2 className='p-2 text-sm border rounded-lg bg-blue-50 text-primary'><strong>Feedback: </strong>{item.feedback}</h2>
           </div>
         </CollapsibleContent>
       </Collapsible>      
